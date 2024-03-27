1a6892456428e98713c950a5280798bdff8581d4
jocelynto <jocelyntoyl@gmail.com>
2024-03-27 17:57:24 +0800

    modified display

--- collab_mri_tasks/stroop-master/stroop-master_lastrun.py
+++ collab_mri_tasks/stroop-master/stroop-master_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2023.1.3),
-    on March 06, 2024, at 11:17
+    on March 27, 2024, at 17:31
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -58,7 +58,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\jocel\\Documents\\collab_mri_tasks\\stroop-master\\stroop-master_lastrun.py',
+    originPath='C:\\Users\\jocel\\Documents\\Collab project Marco Pang\\collab_mri_tasks\\stroop-master\\stroop-master_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
